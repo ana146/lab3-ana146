1ca349d578339ce344b44cf3cdd7cf514c53dc73
anita <anita@10.31.30.78>
2017-11-16 10:39:49 -0400

    bandera definitiva

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,14 +1,17 @@
+
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Ana Campos");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(800, 400);
-		myFrame.setSize(800, 800);
+		myFrame.setLocation(100, 100);
+		myFrame.setSize(400, 300);
         MyPanelClass myPanel = new MyPanelClass();
         myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
 	
+		
+		
 	}
 }
--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -2,16 +2,15 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Insets;
 import java.awt.Polygon;
-
 import javax.swing.JPanel;
  
 public class MyPanelClass extends JPanel {
             /**
-	 * 
-	 */
-	private static final long serialVersionUID = 7057541440811488699L;
-
-			public void paintComponent(Graphics g) {
+     * 
+     */
+    private static final long serialVersionUID = 7057541440811488699L;
+    public static void main(String[] args) {}
+            public void paintComponent(Graphics g) {
                         super.paintComponent(g);
  
                         //Compute interior coordinates
@@ -24,38 +23,39 @@ public class MyPanelClass extends JPanel {
                         int height = y2 - y1;
  
                         //Paint the background
-                        g.setColor(Color.GRAY);
-                        g.fillRect(x1, y1, width , height);
-                        //Draw a border
-                        g.setColor(Color.YELLOW);
-                        g.drawRect(x1, y1, width, height);   
-                        g.setColor(Color.BLUE);
-                        g.drawRect(x1+10, y1+10, width-20, height-20);
                         g.setColor(Color.WHITE);
-                        g.drawLine(x1, y1, x2, y2);
+                      //Paint the rectangles
                         g.setColor(Color.RED);
-                        g.drawLine(getWidth(), 0, 0, getHeight());
-                       // g.setColor(Color.LIGHT_GRAY);
-                       // g.fillOval((getWidth()-55)/2,(getHeight()-55)/2, 55, 55);
+                        g.fillRect(0, 0, width+1, getHeight()/5);     //width +1 para que complete la linea del borde
+                        g.fillRect(0, getHeight()*2/5, width+1, getHeight()/5);     //width +1  para que complete la linea del borde
+                        g.fillRect(0, getHeight()*4/5, width+1, (getHeight()/5)+1); //width +1  para que complete la linea del borde
+                      //Paint the triangles 
+                        Polygon t = new Polygon();
+                        t.addPoint(0,0);
+                        t.addPoint(0,getHeight());
+                        t.addPoint((getWidth()/2),(getHeight()/2));
+                        g.setColor(Color.BLUE);
+                        g.drawPolygon(t);
+                        g.setColor(Color.BLUE);
+                        g.fillPolygon(t);
+                      //Paint the star 
                         Polygon p2 = new Polygon();
-                        p2.addPoint(x1 + 25, y1 + 73);
-                        p2.addPoint(x1 + 41, y1 + 73);
-                        p2.addPoint(x1 + 47, y1 + 58);
-                        p2.addPoint(x1 + 53, y1 + 73);
-                        p2.addPoint(x1 + 69, y1 + 73);
-                        p2.addPoint(x1 + 56, y1 + 83);
-                        p2.addPoint(x1 + 61, y1 + 98);
-                        p2.addPoint(x1 + 47, y1 + 88);
-                        p2.addPoint(x1 + 34, y1 + 98);
-                        p2.addPoint(x1 + 38, y1 + 83);
+                        p2.addPoint((getWidth()/7) + 25, (getHeight()/5) + 73);
+                        p2.addPoint((getWidth()/7) + 41, (getHeight()/5) + 73);
+                        p2.addPoint((getWidth()/7) + 47, (getHeight()/5) + 58);
+                        p2.addPoint((getWidth()/7) + 53, (getHeight()/5) + 73);
+                        p2.addPoint((getWidth()/7) + 69, (getHeight()/5) + 73);
+                        p2.addPoint((getWidth()/7) + 56, (getHeight()/5) + 83);
+                        p2.addPoint((getWidth()/7) + 61, (getHeight()/5) + 98);
+                        p2.addPoint((getWidth()/7) + 47, (getHeight()/5) + 88);
+                        p2.addPoint((getWidth()/7) + 34, (getHeight()/5) + 98);
+                        p2.addPoint((getWidth()/7) + 38, (getHeight()/5) + 83);
                         g.setColor(Color.WHITE);
                         g.drawPolygon(p2);
                         g.setColor(Color.WHITE);
-                         g.fillPolygon(p2);
-
-
+                        g.fillPolygon(p2);
 
                         
                         
-			}
+            }
 }
